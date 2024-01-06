c724bded44470cc444f2f6170c77d8d8c9780ae6
Ryan Dulko <ryandulko458@gmail.com>
2024-01-06 17:07:30 -0300

    Tudo feito

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -8,11 +8,13 @@ describe('Testes da Funcionalidade Usuários', () => {
           cy.token('dudinha123@test.com.br', 'dudoca458').then(tkn => { token = tkn })
      });
 
-     it.only('Deve validar contrato de usuários', () => {
-          cy.request('usuarios').then(response =>{
+
+     it('Deve validar contrato de usuários', () => {
+          cy.request('usuarios').then(response => {
                return contrato.validateAsync(response.body)
           })
      });
+
 
      it('Deve listar usuários cadastrados', () => {
           cy.request({
@@ -20,21 +22,23 @@ describe('Testes da Funcionalidade Usuários', () => {
                url: 'usuarios',
                headers: { authorization: token },
           }).then((response) => {
-               expect(response.body.usuarios[0].nome).to.equal('Eduarda Sartori')
+               expect(response.body.usuarios[1].nome).to.equal('Eduarda Sartori')
                expect(response.status).to.equal(200)
                expect(response.body).to.have.property('usuarios')
                expect(response.duration).to.be.lessThan(20)
           })
      });
 
+
      it('Deve cadastrar um usuário com sucesso', () => {
-          cy.cadastrarUsuarios('denise moraes', 'denisetest@test.com.br', 'testes', 'true')
+          cy.cadastrarUsuarios('carla moraes', 'carlamoraes@test.com.br', 'testa', 'true')
                .then((response) => {
                     expect(response.status).to.equal(201)
                     expect(response.body.message).to.equal('Cadastro realizado com sucesso')
 
                })
      })
+
 
      it('Deve validar um usuário com email inválido', () => {
           cy.cadastrarUsuarios2('ryan suco', 'ryansuco@test.com.br', 'tester', 'true')
@@ -44,6 +48,7 @@ describe('Testes da Funcionalidade Usuários', () => {
 
                })
      });
+
 
      it('Deve editar um usuário previamente cadastrado', () => {
           cy.request('usuarios').then(response => {
@@ -66,29 +71,9 @@ describe('Testes da Funcionalidade Usuários', () => {
 
      });
 
+
      it('Deve deletar um usuário previamente cadastrado', () => {
-          cy.request('usuarios').then(response => {
-               let id = response.body.usuarios._id
-               cy.request({
-                    method: 'DELETE',
-                    url: `usuarios/${id}`,
-                    headers: { authorization: token },
-                    body:
-                    {
-                         "nome": "ryan suco",
-                         "email": "ryansuco@test.com.br",
-                         "password": "tester",
-                         "administrador": "false"
-                    }
-               }).then(response => {
-                    expect(response.body.message).to.equal('Registro excluído com sucesso')
-               })
-          })
-     });
-
-
-     it('Deve deletar usuario', () => {
-          cy.cadastrarUsuarios3(token, 'maria santos','mariasan@test.com.br','testess', 'true')
+          cy.cadastrarUsuarios3(token, 'jogo laoa', 'joaoter@test.com.br', 'testaai', 'true')
                .then(response => {
                     let id = response.body._id
                     cy.request({
